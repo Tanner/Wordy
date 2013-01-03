dabfc3da879958e47f61c280be49805bebd342b0
Tanner Smith <tanner@tssoftware.net>
2013-01-03 16:33:41 -0500

    Calculate various statistics and display them.

--- background.js
+++ background.js
@@ -12,6 +12,13 @@ chrome.contextMenus.onClicked.addListener(function(info, tab) {
     if (!text) {
         alert(chrome.i18n.getMessage("no_text_selected"));
     } else {
-        alert(text);
+        var num_chars = text.length;
+        var num_non_space_chars = text.replace(/ /g, "").length;
+        var num_words = text.split(" ").length;
+
+        var message = chrome.i18n.getMessage("text_statistics", [num_chars, num_non_space_chars, num_words]);
+
+        console.log(message);
+        alert(message);
     }
 });
